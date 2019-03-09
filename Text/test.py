54192f4463977f71929b135172657c75e816da90
KirinNg <kirinng0709@gmail.com>
2019-03-10 03:34:40 +0800

    resnet

---
+++ Text/test.py
@@ -0,0 +1,9 @@
+LABEL_CHOICES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+LABEL_CHOICES_LIST = [str(i) for i in LABEL_CHOICES]
+encode_maps = {}
+decode_maps = {}
+for i, char in enumerate(LABEL_CHOICES, 1):
+    encode_maps[char] = i
+    decode_maps[i] = char
+
+print(LABEL_CHOICES_LIST[15], LABEL_CHOICES_LIST[27])
