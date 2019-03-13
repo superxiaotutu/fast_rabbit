4f5a351fea5e22573015fb43246302243d9b9371
wscjxky <619677632@qq.com>
2019-03-13 09:58:13 +0800

    Default Changelist

--- Text/analyse/train_gauss/train_gauss.py
+++ Text/analyse/train_gauss/train_gauss.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import LSTM_model as LSTM
+import gauss_modeel as LSTM
 import time
 import os
 import datetime
@@ -10,8 +10,8 @@ num_batches_per_epoch = 100
 save_steps = 5000
 validation_steps = 1000
 
-image_height = 60
-image_width = 180
+image_height = 64
+image_width = 192
 image_channel = 3
 
 train_feeder = LSTM.DataIterator()
