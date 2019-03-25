4c4e36009fb0269c7153f1ebe15e52da897ccaeb
wscjxky123 <619677632@qq.com>
2019-03-25 09:11:16 +0800

    i

---
+++ Text/analyse/train_adv/test_adv.py
@@ -0,0 +1,342 @@
+import sys
+import os
+from PIL import Image
+from config import *
+import tensorflow as tf
+import tensorflow.contrib.image
+import tensorflow.contrib.slim as slim
+import time
+
+# from image_process import gene_code
+from gen_type_codes import gene_code_clean
+
+image_channel = 3
+out_channels = 64
+cnn_count = 4
+leakiness = 0.01
+num_hidden = 128
+initial_learning_rate = 0.001
+num_classes = 36 + 2
+
+decay_steps = 4000
+decay_rate = 0.96
+output_keep_prob = 0.8
+
+LABEL_CHOICES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+LABEL_CHOICES_LIST = [str(i) for i in LABEL_CHOICES]
+encode_maps = {}
+decode_maps = {}
+for i, char in enumerate(LABEL_CHOICES, 1):
+    encode_maps[char] = i
+    decode_maps[i] = char
+
+SPACE_INDEX = 0
+SPACE_TOKEN = ''
+encode_maps[SPACE_TOKEN] = SPACE_INDEX
+decode_maps[SPACE_INDEX] = SPACE_TOKEN
+
+
+class LSTMOCR(object):
+    def __init__(self, mode, cnn_mode):
+        self.cnn_mode = cnn_mode
+        self.mode = mode
+        # image
+        self.inputs = tf.placeholder(tf.float32, [None, image_height, image_width, image_channel])
+        # SparseTensor required by ctc_loss op
+        self.labels = tf.sparse_placeholder(tf.int32)
+        # 1d array of size [batch_size]
+        # self.seq_len = tf.placeholder(tf.int32, [None])
+        # l2
+        self._extra_train_ops = []
+
+    def build_graph(self):
+        if self.cnn_mode == "lenet":
+            self._build_model()
+        self._build_train_op()
+
+    def _build_model(self):
+        filters = [3, 64, 128, 128, out_channels]
+        strides = [1, 2]
+
+        feature_h = image_height
+        feature_w = image_width
+
+        count_ = 0
+        min_size = min(image_height, image_width)
+        while min_size > 1:
+            min_size = (min_size + 1) // 2
+            count_ += 1
+        assert (cnn_count <= count_, "FLAGS.cnn_count should be <= {}!".format(count_))
+
+        # CNN part
+        with tf.variable_scope('cnn'):
+            x = self.inputs
+            for i in range(cnn_count):
+                with tf.variable_scope('unit-%d' % (i + 1)):
+                    x = self._conv2d(x, 'cnn-%d' % (i + 1), 3, filters[i], filters[i + 1], strides[0])
+                    x = self._batch_norm('bn%d' % (i + 1), x)
+                    x = self._leaky_relu(x, leakiness)
+                    x = self._max_pool(x, 2, strides[1])
+
+                    # print('----x.get_shape().as_list(): {}'.format(x.get_shape().as_list()))
+                    _, feature_h, feature_w, _ = x.get_shape().as_list()
+                    print('\nfeature_h: {}, feature_w: {}'.format(feature_h, feature_w))
+
+        # LSTM part
+        with tf.variable_scope('lstm'):
+            x = tf.transpose(x, [0, 2, 1, 3])  # [batch_size, feature_w, feature_h, FLAGS.out_channels]
+            # treat `feature_w` as max_timestep in lstm.
+            x = tf.reshape(x, [batch_size, feature_w, feature_h * out_channels])
+            print('lstm input shape: {}'.format(x.get_shape().as_list()))
+            self.seq_len = tf.fill([x.get_shape().as_list()[0]], feature_w)
+            # print('self.seq_len.shape: {}'.format(self.seq_len.shape.as_list()))
+
+            # tf.nn.rnn_cell.RNNCell, tf.nn.rnn_cell.GRUCell
+            cell = tf.nn.rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
+            if self.mode == 'train':
+                cell = tf.nn.rnn_cell.DropoutWrapper(cell=cell, output_keep_prob=output_keep_prob)
+
+            cell1 = tf.nn.rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
+            if self.mode == 'train':
+                cell1 = tf.nn.rnn_cell.DropoutWrapper(cell=cell1, output_keep_prob=output_keep_prob)
+
+            # Stacking rnn cells
+            stack = tf.nn.rnn_cell.MultiRNNCell([cell, cell1], state_is_tuple=True)
+            initial_state = stack.zero_state(batch_size, dtype=tf.float32)
+
+            # The second output is the last state and we will not use that
+            outputs, _ = tf.nn.dynamic_rnn(
+                cell=stack,
+                inputs=x,
+                sequence_length=self.seq_len,
+                initial_state=initial_state,
+                dtype=tf.float32,
+                time_major=False
+            )  # [batch_size, max_stepsize, FLAGS.num_hidden]
+
+            # Reshaping to apply the same weights over the timesteps
+            print(outputs.shape)
+
+            outputs = tf.reshape(outputs, [-1, num_hidden])  # [batch_size * max_stepsize, FLAGS.num_hidden]
+
+            W = tf.get_variable(name='W_out',
+                                shape=[num_hidden, num_classes],
+                                dtype=tf.float32,
+                                initializer=tf.glorot_uniform_initializer())  # tf.glorot_normal_initializer
+            b = tf.get_variable(name='b_out',
+                                shape=[num_classes],
+                                dtype=tf.float32,
+                                initializer=tf.constant_initializer())
+
+            self.logits = tf.matmul(outputs, W) + b
+            print(self.logits)
+
+            # Reshaping back to the original shape
+            shape = tf.shape(x)
+            self.logits = tf.reshape(self.logits, [shape[0], -1, num_classes])
+            print(self.logits)
+
+            # Time major
+            self.logits = tf.transpose(self.logits, (1, 0, 2))
+            print(self.logits)
+
+    def _build_train_op(self):
+        # self.global_step = tf.Variable(0, trainable=False)
+        self.global_step = tf.train.get_or_create_global_step()
+
+        self.loss = tf.nn.ctc_loss(labels=self.labels,
+                                   inputs=self.logits,
+                                   sequence_length=self.seq_len)
+        self.cost = tf.reduce_mean(self.loss)
+        tf.summary.scalar('cost', self.cost)
+
+        self.lrn_rate = tf.train.exponential_decay(initial_learning_rate,
+                                                   self.global_step,
+                                                   decay_steps,
+                                                   decay_rate,
+                                                   staircase=True)
+        tf.summary.scalar('learning_rate', self.lrn_rate)
+
+        # self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.lrn_rate,
+        #                                            momentum=FLAGS.momentum).minimize(self.cost,
+        #                                                                              global_step=self.global_step)
+        # self.optimizer = tf.train.MomentumOptimizer(learning_rate=self.lrn_rate,
+        #                                             momentum=FLAGS.momentum,
+        #                                             use_nesterov=True).minimize(self.cost,
+        #                                                                         global_step=self.global_step)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.lrn_rate).minimize(self.loss,
+                                                                                      global_step=self.global_step)
+        train_ops = [self.optimizer] + self._extra_train_ops
+        self.train_op = tf.group(*train_ops)
+
+        # Option 2: tf.nn.ctc_beam_search_decoder
+        # (it's slower but you'll get better results)
+        self.decoded, self.log_prob = tf.nn.ctc_beam_search_decoder(self.logits, self.seq_len, merge_repeated=False)
+        # self.decoded, self.log_prob = tf.nn.ctc_greedy_decoder(logits, seq_len, merge_repeated=False)
+        self.dense_decoded = tf.sparse_tensor_to_dense(self.decoded[0], default_value=-1)
+
+    def _conv2d(self, x, name, filter_size, in_channels, out_channels, strides):
+        with tf.variable_scope(name):
+            kernel = tf.get_variable(name='W',
+                                     shape=[filter_size, filter_size, in_channels, out_channels],
+                                     dtype=tf.float32,
+                                     initializer=tf.glorot_uniform_initializer())  # tf.glorot_normal_initializer
+
+            b = tf.get_variable(name='b',
+                                shape=[out_channels],
+                                dtype=tf.float32,
+                                initializer=tf.constant_initializer())
+
+            con2d_op = tf.nn.conv2d(x, kernel, [1, strides, strides, 1], padding='SAME')
+
+        return tf.nn.bias_add(con2d_op, b)
+
+    def _batch_norm(self, name, x):
+        """Batch normalization."""
+        with tf.variable_scope(name):
+            x_bn = tf.contrib.layers.batch_norm(
+                inputs=x,
+                decay=0.9,
+                center=True,
+                scale=True,
+                epsilon=1e-5,
+                updates_collections=None,
+                is_training=self.mode == 'train',
+                fused=True,
+                data_format='NHWC',
+                zero_debias_moving_mean=True,
+                scope='BatchNorm'
+            )
+        return x_bn
+
+    def _leaky_relu(self, x, leakiness=0.0):
+        return tf.where(tf.less(x, 0.0), leakiness * x, x, name='leaky_relu')
+
+    def _max_pool(self, x, ksize, strides):
+        return tf.nn.max_pool(x,
+                              ksize=[1, ksize, ksize, 1],
+                              strides=[1, strides, strides, 1],
+                              padding='SAME',
+                              name='max_pool')
+
+    def _avg_pool(self, x, ksize, strides):
+        return tf.nn.avg_pool(x,
+                              ksize=[1, ksize, ksize, 1],
+                              strides=[1, strides, strides, 1],
+                              padding='SAME',
+                              name='avg_pool')
+
+arr=[]
+shuff_dir = {}
+lst = [i for i in range(36)]
+random.shuffle(lst)
+
+
+def creat_onehot(num):
+    re = np.zeros([38])
+    re[num - 1] = 1
+    return re
+
+
+for i in range(36):
+    shuff_dir.update({i + 1: creat_onehot(lst[i])})
+shuff_dir.update({37: creat_onehot(37)})
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+model = LSTMOCR("test", "lenet")
+model.build_graph()
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+target = tf.placeholder(tf.float32, [12, batch_size, 38])
+origin_inputs = tf.placeholder(tf.float32, [None, image_height, image_width, image_channel])
+predict = tf.nn.softmax(model.logits)
+# ADV_LOSS = tf.reduce_sum(tf.square(predict - target)) + tf.reduce_mean(tf.square(origin_inputs - model.inputs))
+current_status = tf.argmax(predict, axis=-1)
+current_mengban = tf.one_hot(current_status, 38, axis=0)
+current_mengban = tf.transpose(current_mengban, [1, 2, 0])
+
+ADV_LOSS = tf.reduce_mean(tf.square(origin_inputs - model.inputs)) + tf.reduce_mean(
+    tf.square(tf.reduce_sum(predict * current_mengban) - tf.reduce_sum(predict * target)))
+
+grad_y2x = tf.sign(tf.gradients(ADV_LOSS, model.inputs)[0])
+
+Var_restore = tf.global_variables()
+saver = tf.train.Saver(Var_restore, max_to_keep=5, allow_empty=True)
+sess = tf.Session(config=config)
+sess.run(tf.global_variables_initializer())
+
+ckpt = tf.train.latest_checkpoint('train_lenet_clean/model')
+if ckpt:
+    saver.restore(sess, ckpt)
+    print('restore from ckpt{}'.format(ckpt))
+else:
+    print('cannot restore')
+
+def creat_adv(img_PATH):
+    im = np.asarray(img_PATH).astype(np.float32) / 255.
+    imgs_input = []
+    imgs_input.append(im)
+    imgs_input = np.repeat(imgs_input, batch_size, axis=0)
+
+    imgs_input_before = imgs_input
+    feed = {model.inputs: imgs_input}
+
+    log = sess.run(model.logits, feed)
+    fir = log[:, 0, :]
+    ex = np.argmax(fir, axis=1)
+
+    target_creat = []
+    for e in range(12):
+        target_creat.append(shuff_dir[ex[e]])
+
+    target_creat = np.asarray(target_creat)
+    target_creat = target_creat[:, np.newaxis, :]
+    target_creat = np.repeat(target_creat, batch_size, axis=1)
+
+    dense_decoded_code = sess.run(model.dense_decoded, feed)
+    expression = ''
+    for c in dense_decoded_code[0]:
+        if c == -1:
+            expression += '-'
+        else:
+            expression += decode_maps[c]
+    expression_before = expression
+    print("BEFORE:{}".format(expression))
+
+    # 0.09
+    #0.004
+    adv_step = 0
+    # 50
+
+    acc = [0 for i in range(50)]
+    for item in range(50):
+        feed = {model.inputs: imgs_input, target: target_creat, origin_inputs: imgs_input_before}
+        loss_now, grad = sess.run([ADV_LOSS, grad_y2x], feed)
+        if (item + 1) % 10 == 0:
+            print("LOSS:{}".format(loss_now))
+        imgs_input = imgs_input - grad * adv_step
+        # plt.imshow(imgs_input[0])
+        # plt.imsave(str(item) + 'level.png', imgs_input[0])
+        feed = {model.inputs: imgs_input, target: target_creat, origin_inputs: imgs_input_before}
+        dense_decoded_code = sess.run(model.dense_decoded, feed)
+        expression = ''
+        for i in dense_decoded_code[0]:
+            if i == -1:
+                expression += ''
+            else:
+                expression += decode_maps[i]
+        if expression == expression_before:
+            acc[item] += 1
+        print("AFTER:{}".format(expression))
+    arr.append(acc)
+    return
+
+
+if __name__ == '__main__':
+    for i in range(1000):
+        slice = random.sample(LABEL_CHOICES_LIST, 4)
+        captcha = ''.join(slice)
+        img = gene_code_clean(captcha)
+        creat_adv( img)
+        print(arr)
