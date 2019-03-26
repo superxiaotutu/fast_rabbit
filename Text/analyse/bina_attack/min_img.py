cec12b693a471e17c16c3c576e5490e4a657152d
wscjxky123 <619677632@qq.com>
2019-03-27 01:02:04 +0800

    i

--- Text/analyse/bina_attack/min_img.py
+++ Text/analyse/bina_attack/min_img.py
@@ -1,24 +1,29 @@
+import cv2
+
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    imgs=[]
-    a = np.asarray(Image.open('images/bin_adv_0.png', 'r').convert('L'))
-    b = np.asarray(Image.open('images/bin_adv_49.png', 'r').convert('L'))
-    b_bin = b.copy()
-    b_bin[b_bin >= np.mean(b_bin)] = 255
-    b_bin[b_bin < np.mean(b_bin)] = 0
-    c = b-a
-    c_bin =b_bin-a
-    imgs.append(a)
-    imgs.append(b)
+    imgs = []
+    ori = cv2.imread('images/46_A.png')
+    ori = cv2.cvtColor(ori, cv2.COLOR_BGR2RGB)
+    adv = cv2.imread('images/23_40_A.png')
+    adv = cv2.cvtColor(adv, cv2.COLOR_BGR2RGB)
+    b_bin = cv2.imread('images/9.png')
+    c = (ori - adv)*2
+    c = cv2.cvtColor(c, cv2.COLOR_RGB2GRAY)
+    retval, c_bin = cv2.threshold(c, 0, 255, cv2.THRESH_OTSU)
+    imgs.append(ori)
+    imgs.append(adv)
     imgs.append(b_bin)
     imgs.append(c)
     imgs.append(c_bin)
-
-    for i,v in enumerate(imgs):
-        plt.subplot(1, 5, i+1)
-        plt.imshow(v)
+    print(c_bin)
+    # cv2.imshow('s',c)
+    # cv2.waitKey(0)
+    for i, v in enumerate(imgs):
+        plt.subplot(1, 5, i + 1)
+        plt.imshow(v,cmap='gray')
         plt.axis("off")
     plt.show()
