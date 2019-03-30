fdc197ba3fbe3ed0152682b8a9e1fb6c91293c61
KirinNg <kirinng0709@gmail.com>
2019-03-30 22:32:27 +0800

    ii

--- Text/experiment/attack_dete/ocr_line.py
+++ Text/experiment/attack_dete/ocr_line.py
@@ -1,4 +1,6 @@
 import glob
+import time
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 # plt.switch_backend('agg')
@@ -14,6 +16,65 @@ from gen_type_codes import *
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 RELEASE = True
+adv_step = 0.90
+adv_count = 100
+
+def attack(model,sess,imgs_input,type):
+        # adv_node
+    shuff_dir = {}
+    lst = [i for i in range(36)]
+    random.shuffle(lst)
+
+    def creat_onehot(num):
+        re = np.zeros([38])
+        re[num - 1] = 1
+        return re
+    for i in range(36):
+        shuff_dir.update({i + 1: creat_onehot(lst[i])})
+    shuff_dir.update({37: creat_onehot(37)})
+    target = tf.placeholder(tf.float32, [12, batch_size, 38])
+    CNN_target = tf.placeholder(tf.float32, [batch_size, 4, 38])
+    origin_inputs = tf.placeholder(tf.float32, [None, image_height, image_width, image_channel])
+    predict = tf.nn.softmax(model.logits)
+    # ADV_LOSS = tf.reduce_sum(tf.square(predict - target)) + tf.reduce_mean(tf.square(origin_inputs - model.inputs))
+    current_status = tf.argmax(predict, axis=-1)
+    current_mengban = tf.one_hot(current_status, 38, axis=0)
+    current_mengban = tf.transpose(current_mengban, [1, 2, 0])
+    if type == 'ocr':
+        ADV_LOSS = tf.reduce_mean(tf.square(origin_inputs - model.inputs)) + tf.reduce_mean(
+                tf.reduce_sum(predict * target)-tf.reduce_sum(predict * current_mengban) )
+    else:
+        ADV_LOSS = tf.reduce_mean(tf.square(origin_inputs - model.inputs)) + tf.reduce_mean(
+            tf.reduce_sum(predict * target) - tf.reduce_sum(predict * current_mengban))
+
+    grad_y2x = tf.gradients(ADV_LOSS, model.inputs)[0]
+
+    imgs_input_before = imgs_input
+    feed = {model.inputs: imgs_input}
+    log = sess.run(model.logits, feed)
+    fir = log[:, 0, :]
+    ex = np.argmax(fir, axis=1)
+    target_creat = []
+    start=time.time()
+    for i in range(12):
+        target_creat.append(shuff_dir[ex[i]])
+    target_creat = np.asarray(target_creat)
+    target_creat = target_creat[:, np.newaxis, :]
+    target_creat = np.repeat(target_creat, batch_size, axis=1)
+    feed = {model.inputs: imgs_input, target: target_creat, origin_inputs: imgs_input_before}
+    for i in range(adv_count):
+        loss_now, grad = sess.run([ADV_LOSS, grad_y2x], feed)
+        if (i + 1) % 10 == 0:
+            print("LOSS:{}".format(np.max(grad)))
+        imgs_input = imgs_input - grad * adv_step
+        imgs_input = np.clip(imgs_input, 0, 1)
+        feed = {model.inputs: imgs_input, target: target_creat, origin_inputs: imgs_input_before}
+    imgs_input_after = imgs_input
+    end = time.time()
+    distance=np.linalg.norm(imgs_input_after-imgs_input_before)
+    cost_time=end-start
+
+    return imgs_input_after
 
 
 def cnn_generate(Checkpoint_PATH, model_name):
@@ -63,8 +124,7 @@ def cnn_generate(Checkpoint_PATH, model_name):
     else:
         print('cannot restore')
         return
-    adv_step = 0.90
-    adv_count = 100
+
     file_count = 1000
     with open('cnn_result.txt', 'w')as f:
         for type in range(4):
@@ -201,8 +261,6 @@ def ocr_generate(Checkpoint_PATH, model_name):
     else:
         print('cannot restore')
         return
-    adv_step = 0.90
-    adv_count = 100
     file_count = 1000
     with open('ocr_result.txt', 'w')as f:
         for type in range(4):
